b9c0fb58a4f1f290b39ad51e7a35ef004a707cb0
Benjamin Isong <beisongabc@gmail.com>
2020-05-31 16:15:48 +0100

    bug fix

--- database/seeds/AdminsTableSeeder.php
+++ database/seeds/AdminsTableSeeder.php
@@ -2,6 +2,7 @@
 
 use App\Models\Admin;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class AdminsTableSeeder extends Seeder
 {
@@ -12,6 +13,22 @@ class AdminsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(Admin::class, 1)->create();
+        $admin = new Admin();
+        $admin->who = 4;
+        $admin->uuid = (string) Str::uuid();
+        $admin->title = "Mr";
+        $admin->first_name = "Super";
+        $admin->last_name = "Admin";
+        $admin->email = 'admin@niucms.com'; //$faker->unique()->safeEmail;
+        $admin->phone = "090Admin";
+        $admin->address = "Not Updated";
+        $admin->active = true;
+        $admin->password = bcrypt('password');
+        $admin->last_seen = time();
+        $admin->dob = null;
+        $admin->theme_type = 'light';
+        $admin->email_verified_at = now();
+        $admin->remember_token = Str::random(10);
+        $admin->save();
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Models\Admin;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
@@ -13,6 +15,22 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
 
-
+        $admin = new Admin();
+        $admin->who = 4;
+        $admin->uuid = (string) Str::uuid();
+        $admin->title = "Mr";
+        $admin->first_name = "Super";
+        $admin->last_name = "Admin";
+        $admin->email = 'admin@niucms.com'; //$faker->unique()->safeEmail;
+        $admin->phone = "090Admin";
+        $admin->address = "Not Updated";
+        $admin->active = true;
+        $admin->password = bcrypt('password');
+        $admin->last_seen = time();
+        $admin->dob = null;
+        $admin->theme_type = 'light';
+        $admin->email_verified_at = now();
+        $admin->remember_token = Str::random(10);
+        $admin->save();
     }
 }
